8e813a6953162a5c475fa0f71af8f007d20c14db
yihan <yihanshang0@gmail.com>
2025-02-26 17:24:05 +0800

    Restruct

--- main.go
+++ main.go
@@ -1,30 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	for {
-		scanner := bufio.NewScanner(os.Stdin)
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := scanner.Text()
-		formatted := cleanInput(input)[0]
-		fmt.Printf("Your command was: %v\n", formatted)
-	}
-}
-
-func cleanInput(text string) []string {
-	// Trim leading/trailing whitespace and convert the text to lowercase
-	text = strings.TrimSpace(text)
-	text = strings.ToLower(text)
-
-	// Split the text by whitespace into a slice of words
-	words := strings.Fields(text)
-
-	return words
+	startRepl()
 }
